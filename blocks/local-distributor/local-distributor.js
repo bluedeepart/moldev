2ebf324c64384fbc4dc7e96e08e00d0dfec66d73
Deepti Rathor <deepti.rathor46@gmail.com>
2023-03-23 14:45:54 +0530

    PSI

--- blocks/local-distributor/local-distributor.js
+++ blocks/local-distributor/local-distributor.js
@@ -1,20 +1,45 @@
 import ffetch from '../../scripts/ffetch.js';
 
+function getSelectOptions(rows) {
+  return rows.map((value) => `<option value='${value}'>${value}</option>`);
+}
+
+function searchDistributorForm(countryList, productFamilyList) {
+  return `<div class="form">
+			<div class="form-group">
+				<div class="fields">
+					<div class="select-wrapper">
+						<select name="country" id="country" class="form-control" required="">
+							<option value="">Select Region/Country</option>
+							${getSelectOptions(countryList)}
+						</select>
+						<span class="fa fa-chevron-down"></span>
+					</div>
+					<div class="select-wrapper">
+						<select name="product_family" id="product_family" class="form-control">
+							<option value="">Select Product Group</option>
+              ${getSelectOptions(productFamilyList)}
+						</select>
+						<span class="fa fa-chevron-down"></span>
+					</div>
+				</div>
+				<div class="button" id="searchButton">
+					<button>SEARCH</button>
+				</div>
+			</div>
+		</div>
+  `;
+}
+
 export default async function decorate(block) {
-  const heading = block.querySelector('h5');
-  const cloneHeading = heading.cloneNode(true);
-  heading.remove();
-  block.insertBefore(cloneHeading, block.firstChild);
-
-  const searchResult = document.createElement('div');
-  searchResult.setAttribute('class', 'search-result');
-
   const distributors = await ffetch('/local-distibutors.json').withFetch(fetch).all();
 
+  const productFamilyList = await ffetch('/test.json')
+    .withFetch(fetch)
+    .map(({ PrimaryProducts }) => PrimaryProducts)
+    .all();
+
   const countryList = [...new Set(distributors.map(({ Country }) => Country))];
-  const countrySelectOptions = countryList.map(
-    (value) => `<option value='${value}'>${value}</option>`,
-  );
 
   const renderAddress = () => {
     let countryName = document.getElementById('country').value;
@@ -30,8 +55,9 @@ export default async function decorate(block) {
       .filter(({ PrimaryProducts }) => PrimaryProducts.includes(productFamily) > 0);
 
     let finalHtml = '';
-    const resultHeading = document.createElement('h3');
+    let resultHeading = document.createElement('h3');
     const searchResultEl = document.querySelector('.local-distributor .search-result');
+
     filterdata.forEach((row) => {
       resultHeading.textContent = row.Country;
       const primeProduct = row.PrimaryProducts.replace(/,/g, ' | ');
@@ -42,26 +68,41 @@ export default async function decorate(block) {
         ? `<a href="${row.Link}" target="_blank" rel="noopener noreferrer">Online Support Request</a>`
         : '';
 
+      row.PrimaryProducts.length === 0 && row.Address.length === 0
+        ? (resultHeading.style.display = 'none')
+        : (resultHeading.style.display = 'block');
+
       let newStr = '';
       row.Address.split(' ').forEach((add) => {
         if (add.indexOf(':') > -1) {
           newStr += add.replace(add, ` <strong>${add}</strong> `);
         } else {
-          newStr += add;
+          newStr += `${add} `;
         }
       });
       const molAddress = `${newStr.replace(/\n/g, '<br>')}<br>`;
 
+      console.log(resultHeading);
+      console.log('primeProduct');
+      console.log(row.PrimaryProducts.length);
+      console.log('molAddress');
+      console.log(row.Address.length);
+
       /* eslint no-tabs: ["error", { allowIndentationTabs: true }] */
       finalHtml += `
-					<div class="search-result-content ${customClass}-result">
-						<div class="type">${row.Type}</div>
-            <div class="productfamily">${primeProduct}</div>
-            <div class="address">
-              ${molAddress}
-              ${supportLink}
-            </div>
-						<p><a href="#">Contact your local ${row.Type} Team</a></p>
+					<div class="search-result-content ${customClass ? customClass : 'no'}-result">
+            ${
+              row.PrimaryProducts.length === 0 && row.Address.length === 0
+                ? `<h3>NO RESULT FOUND</h3>`
+                : ` <div class="type">${row.Type}</div>
+                <div class="productfamily">${primeProduct}</div>
+                <div class="address">
+                  ${molAddress}
+                  ${supportLink}
+                </div>
+                <p><a href="#">Contact your local ${row.Type} Team</a></p>
+              `
+            }
 					</div>
 				`;
     });
@@ -70,39 +111,13 @@ export default async function decorate(block) {
   };
 
   /* eslint no-tabs: ["error", { allowIndentationTabs: true }] */
-  const formWrapper = `
-		<div class="form">
-			<div class="form-group">
-				<div class="fields">
-					<div class="select-wrapper">
-						<select name="country" id="country" class="form-control" required="">
-							<option value="">Select Region/Country</option>
-							${countrySelectOptions}
-						</select>
-						<span class="fa fa-chevron-down"></span>
-					</div>
-					<div class="select-wrapper">
-						<select name="product_family" id="product_family" class="form-control">
-							<option value="">Select Product Group</option>
-							<option value="Assay Kits, Media, Reagents"> Assay Kits, Media, Reagents</option>
-							<option value="Axon/Patch Clamp"> Axon/Patch Clamp</option>
-							<option value="Cellular Imaging Systems"> Cellular Imaging Systems</option>
-							<option value="Clone Screening Systems"> Clone Screening Systems</option>
-							<option value="MetaMorph"> MetaMorph</option>
-							<option value="Microplate Readers"> Microplate Readers</option>
-							<option value="Threshold High Throughput Screening"> Threshold High Throughput Screening</option>
-							<option value="Washers and Handlers"> Washers and Handlers</option>
-						</select>
-						<span class="fa fa-chevron-down"></span>
-					</div>
-				</div>
-				<div class="button" id="searchButton">
-					<button>SEARCH</button>
-				</div>
-			</div>
-		</div>
-  `;
-
+  const heading = block.querySelector('h5');
+  const cloneHeading = heading.cloneNode(true);
+  heading.remove();
+  block.insertBefore(cloneHeading, block.firstChild);
+  const searchResult = document.createElement('div');
+  searchResult.setAttribute('class', 'search-result');
+  const formWrapper = searchDistributorForm(countryList, productFamilyList);
   document.querySelector('.local-distributor > div').lastElementChild.innerHTML = formWrapper;
   document.querySelector('.local-distributor').appendChild(searchResult);
   const searchButton = document.getElementById('searchButton');
