b5cffa60309c1008cb5d6d3eb8d76e39793d74ff
Deepti Rathor <deepti.rathor46@gmail.com>
2023-03-31 13:05:14 +0530

    31st march

--- blocks/regional-contacts/regional-contacts.js
+++ blocks/regional-contacts/regional-contacts.js
@@ -147,7 +147,7 @@ const updatedTxt =
   '<a href="javascript:void(0);" title="Contact Local Team">Contact Local Team</a>';
 
 /* eslint no-return-assign: ["error", "always"] */
-localTeamText.forEach(
-  (localText) => (localText.innerHTML = localText.innerHTML.replaceAll(txt, updatedTxt)),
+localText.innerHTML = localTeamText.forEach((localText) =>
+  localText.innerHTML.replaceAll(txt, updatedTxt),
 );
 /* ================ TAB HANDLER ===================== */
