be461afe09276bca2a3d52631f9ba7e7e966be58
Deepti Rathor <31531837+bluedeepart@users.noreply.github.com>
2023-05-01 15:36:15 +0530

    vidyard-changes

--- blocks/vidyard/vidyard.js
+++ blocks/vidyard/vidyard.js
@@ -1,36 +1,45 @@
 import { loadScript } from '../../scripts/scripts.js';
 
+function isExistScript(url) {
+	const script = document.querySelector(`script[src="${url}"]`);
+	if (script === null) {
+		loadScript(url);
+	}
+}
+
 export default function decorate(block) {
-  const videoUrl = block.querySelector('a');
-  if (videoUrl) {
-    const videoId = videoUrl.href.substring(videoUrl.href.lastIndexOf('/') + 1);
-    const thumbnail = block.querySelector('img');
-    if (thumbnail) {
-      const thumbObserver = new IntersectionObserver((entries) => {
-        if (entries.some((e) => e.isIntersecting)) {
-          thumbObserver.disconnect();
-          loadScript('https://play.vidyard.com/embed/v4.js');
-          thumbnail.style = 'width: 100%; margin: auto; display: block;';
-          thumbnail.classList.add('vidyard-player-embed');
-          thumbnail.setAttribute('data-uuid', videoId);
-          thumbnail.setAttribute('data-v', '4');
-          thumbnail.setAttribute('data-width', block.classList.contains('lightbox') ? '700' : '');
-          thumbnail.setAttribute('data-height', block.classList.contains('lightbox') ? '394' : '');
-          thumbnail.setAttribute('data-autoplay', block.classList.contains('lightbox') ? '1' : '0');
-          thumbnail.setAttribute(
-            'data-type',
-            block.classList.contains('lightbox') ? 'lightbox' : 'inline',
-          );
-          videoUrl.remove();
-        }
-      });
-      thumbObserver.observe(thumbnail);
-    } else {
-      const observer = new IntersectionObserver((entries) => {
-        if (entries.some((e) => e.isIntersecting)) {
-          observer.disconnect();
-          loadScript('https://play.vidyard.com/embed/v4.js');
-          block.innerHTML = `<img style="width: 100%; margin: auto; display: block;"
+	const videoUrl = block.querySelector('a');
+	if (videoUrl) {
+		const videoId = videoUrl.href.substring(videoUrl.href.lastIndexOf('/') + 1);
+		const thumbnail = block.querySelector('img');
+		if (thumbnail) {
+			const thumbObserver = new IntersectionObserver((entries) => {
+				if (entries.some((e) => e.isIntersecting)) {
+					thumbObserver.disconnect();
+					loadScript('https://play.vidyard.com/embed/v4.js');
+					//isExistScript('https://play.vidyard.com/embed/v4.js');
+					thumbnail.style = 'width: 100%; margin: auto; display: block;';
+					thumbnail.classList.add('vidyard-player-embed');
+					thumbnail.setAttribute('data-uuid', videoId);
+					thumbnail.setAttribute('data-v', '4');
+					thumbnail.setAttribute('data-width', block.classList.contains('lightbox') ? '700' : '');
+					thumbnail.setAttribute('data-height', block.classList.contains('lightbox') ? '394' : '');
+					thumbnail.setAttribute('data-autoplay', block.classList.contains('lightbox') ? '1' : '0');
+					thumbnail.setAttribute(
+						'data-type',
+						block.classList.contains('lightbox') ? 'lightbox' : 'inline',
+					);
+					videoUrl.remove();
+				}
+			});
+			thumbObserver.observe(thumbnail);
+		} else {
+			const observer = new IntersectionObserver((entries) => {
+				if (entries.some((e) => e.isIntersecting)) {
+					observer.disconnect();
+					loadScript('https://play.vidyard.com/embed/v4.js');
+					//isExistScript('https://play.vidyard.com/embed/v4.js');
+					block.innerHTML = `<img style="width: 100%; margin: auto; display: block;"
             class="vidyard-player-embed"
             src="https://play.vidyard.com/${videoId}.jpg"
             data-uuid="${videoId}"
@@ -39,9 +48,9 @@ export default function decorate(block) {
             data-height="${block.classList.contains('lightbox') ? '394' : ''}"
             data-autoplay="${block.classList.contains('lightbox') ? '1' : '0'}"
             data-type="${block.classList.contains('lightbox') ? 'lightbox' : 'inline'}"/>`;
-        }
-      });
-      observer.observe(block);
-    }
-  }
+				}
+			});
+			observer.observe(block);
+		}
+	}
 }
