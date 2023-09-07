4583e93bd613700fae23b346b95098423abc40d9
Deepti Rathor <31531837+bluedeepart@users.noreply.github.com>
2023-09-07 14:32:00 +0530

    accordion

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -18,8 +18,10 @@ function applyColumnLayout(contentNodes) {
 
 function renderColumnLayout(row) {
   const picture = row[0];
-  const text = row[1];
-  const link = row[2];
+  const textArr = row.slice(1, -1);
+  const link = row[row.length - 1];
+  let text = div();
+  textArr.forEach(t => text.appendChild(t));
   if (link) link.querySelector('a').append(span({ class: 'icon icon-fa-arrow-circle-right' }));
 
   const leftCol = div({ class: 'accordion-content-col-left' }, picture);
