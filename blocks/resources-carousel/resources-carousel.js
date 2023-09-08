9711e5704452465627104a6ccbfe5105722b4d45
Deepti Rathor <31531837+bluedeepart@users.noreply.github.com>
2023-09-08 16:38:56 +0530

    resources-carousel

--- blocks/resources-carousel/resources-carousel.js
+++ blocks/resources-carousel/resources-carousel.js
@@ -4,63 +4,63 @@ import { createCarousel } from '../carousel/carousel.js';
 import { createCard } from '../card/card.js';
 
 async function getFeaturedResources(paths) {
-	return ffetch('/query-index.json')
-		.sheet('resources')
-		.chunks(2000)
-		.filter(
-			(resource) =>
-				paths.includes(resource.path) ||
-				paths.includes(resource.gatedURL) ||
-				(resource.gatedURL &&
-					resource.gatedURL !== '0' &&
-					paths.includes(new URL(resource.gatedURL, 'https://moleculardevices.com').pathname)),
-		)
-		.limit(9)
-		.all();
+  return ffetch('/query-index.json')
+    .sheet('resources')
+    .chunks(2000)
+    .filter(
+      (resource) =>
+        paths.includes(resource.path) ||
+        paths.includes(resource.gatedURL) ||
+        (resource.gatedURL &&
+          resource.gatedURL !== '0' &&
+          paths.includes(new URL(resource.gatedURL, 'https://moleculardevices.com').pathname)),
+    )
+    .limit(9)
+    .all();
 }
 
 export default async function decorate(block) {
-	const blockLinks = block.querySelectorAll('a');
-	let resources = [];
+  const blockLinks = block.querySelectorAll('a');
+  let resources = [];
 
-	if (blockLinks.length !== 0) {
-		resources = await getFeaturedResources(
-			[...blockLinks].map((link) => link.getAttribute('href')),
-		);
-	}
+  if (blockLinks.length !== 0) {
+    resources = await getFeaturedResources(
+      [...blockLinks].map((link) => link.getAttribute('href')),
+    );
+  }
 
-	const placeholders = await fetchPlaceholders();
+  const placeholders = await fetchPlaceholders();
 
-	const resourceCard = await createCard({
-		titleLink: false,
-		thumbnailLink: false,
-		defaultButtonText: placeholders.learnMore || 'Learn more',
-		descriptionLength: block.classList.contains('list') ? 180 : 180,
-	});
+  const resourceCard = await createCard({
+    titleLink: false,
+    thumbnailLink: false,
+    defaultButtonText: placeholders.learnMore || 'Learn more',
+    descriptionLength: block.classList.contains('list') ? 180 : 180,
+  });
 
-	await createCarousel(block, resources, {
-		cssFiles: ['/blocks/carousel/carousel-cards.css'],
-		defaultStyling: true,
-		navButtons: true,
-		dotButtons: false,
-		infiniteScroll: true,
-		autoScroll: false,
-		visibleItems: [
-			{
-				items: 1,
-				condition: () => window.screen.width < 768,
-			},
-			{
-				items: 2,
-				condition: () => window.screen.width < 1200,
-			},
-			{
-				items: 3,
-			},
-		],
-		cardRenderer: resourceCard,
-	});
+  await createCarousel(block, resources, {
+    cssFiles: ['/blocks/carousel/carousel-cards.css'],
+    defaultStyling: true,
+    navButtons: true,
+    dotButtons: false,
+    infiniteScroll: true,
+    autoScroll: false,
+    visibleItems: [
+      {
+        items: 1,
+        condition: () => window.screen.width < 768,
+      },
+      {
+        items: 2,
+        condition: () => window.screen.width < 1200,
+      },
+      {
+        items: 3,
+      },
+    ],
+    cardRenderer: resourceCard,
+  });
 
-	block.parentElement.classList.add('carousel-wrapper');
-	// block.classList.add('cards');
+  block.parentElement.classList.add('carousel-wrapper');
+  // block.classList.add('cards');
 }
