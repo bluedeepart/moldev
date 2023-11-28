29008684f57d44b6913364eff117a47940a3aaae
Deepti Rathor <31531837+bluedeepart@users.noreply.github.com>
2023-11-28 18:31:41 +0530

    new

--- blocks/featured-products-carousel/featured-products-carousel.js
+++ blocks/featured-products-carousel/featured-products-carousel.js
@@ -3,7 +3,6 @@ import { fetchPlaceholders } from '../../scripts/lib-franklin.js';
 import { createCard } from '../card/card.js';
 import { createCarousel } from '../carousel/carousel.js';
 
-
 const resourceCard = await createCard({
   defaultButtonText: fetchPlaceholders.details || 'Details',
   titleLink: false,
@@ -28,19 +27,18 @@ const miniStyleConfig = {
 };
 
 async function getCbValues(link) {
-  return await ffetch('/query-index.json')
-  .sheet('products')
-  .filter((post) => link.indexOf(post.path) > -1)
-  .all();
+  return ffetch('/query-index.json')
+    .sheet('products')
+    .filter((post) => link.indexOf(post.path) > -1)
+    .all();
 }
 
 export default async function decorate(block) {
   const miniStyle = block.classList.contains('mini');
   if (miniStyle) {
     let products = [];
-    const product_links = block.querySelectorAll('a');
-    products = await getCbValues([...product_links].map((link) => link.getAttribute('href')));
+    const productLinks = block.querySelectorAll('a');
+    products = await getCbValues([...productLinks].map((link) => link.getAttribute('href')));
     await createCarousel(block, products, miniStyleConfig);
-    return;
   }
 }
