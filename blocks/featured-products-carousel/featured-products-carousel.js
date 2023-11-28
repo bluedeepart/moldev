8ed9bbb42b1858aaad743005f17ed9459a3d6976
Deepti Rathor <31531837+bluedeepart@users.noreply.github.com>
2023-11-28 19:22:18 +0530

    new

--- blocks/featured-products-carousel/featured-products-carousel.js
+++ blocks/featured-products-carousel/featured-products-carousel.js
@@ -27,7 +27,7 @@ const miniStyleConfig = {
 };
 
 async function getCbValues(link) {
-  return await ffetch('/query-index.json')
+  return ffetch('/query-index.json')
     .sheet('products')
     .filter((post) => link.indexOf(post.path) > -1)
     .all();
@@ -37,8 +37,8 @@ export default async function decorate(block) {
   const miniStyle = block.classList.contains('mini');
   if (miniStyle) {
     let products = [];
-    const product_links = block.querySelectorAll('a');
-    products = await getCbValues([...product_links].map((link) => link.getAttribute('href')));
+    const productLinks = block.querySelectorAll('a');
+    products = await getCbValues([...productLinks].map((link) => link.getAttribute('href')));
     await createCarousel(block, products, miniStyleConfig);
     return;
   }
